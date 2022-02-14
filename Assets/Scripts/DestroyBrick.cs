3a9fb339d64def063ff47192269184909846eb63
Juliaw02 <98358554+Juliaw02@users.noreply.github.com>
2022-02-13 18:53:05 -0600

    Added Background
    Added assets for all the backgrounds, switched around some of the brick positions, still figuring out the teleporting mask and exploding fish.

--- Assets/Scripts/DestroyBrick.cs
+++ Assets/Scripts/DestroyBrick.cs
@@ -6,27 +6,43 @@ public class DestroyBrick : MonoBehaviour
 {
     public int numberOfHits = 0;
     public int maxHits;
-
     public Sprite noHit;
     public Sprite oneHit;
     public SpriteRenderer brickSprite;
     public int brickValue;
-
     public Transform powerup;
-
     public GameMaster gameMaster;
+
+    //public Vector3[] maskLocations;
 
     // Start is called before the first frame update
     void Start()
     {
         brickSprite = GetComponent<SpriteRenderer>();
         gameMaster.GetComponent<GameMaster>();
+
+       // maskLocations = new Vector3[8];
+       // maskLocations[0] = new Vector3(-7.5f, 1.75f, 0f);
+       // maskLocations[1] = new Vector3(-4.5f, 0.75f, 0f);
+       // maskLocations[2] = new Vector3(-1.5f, -0.25f, 0f);
+       // maskLocations[3] = new Vector3(1.5f, -0.25f, 0f);
+       // maskLocations[4] = new Vector3(7.5f, -0.25f, 0f);
+       // maskLocations[5] = new Vector3(-4.5f, -0.25f, 0f);
+       // maskLocations[6] = new Vector3(-7.5f, 0.75f, 0f);
+       // maskLocations[7] = new Vector3(-7.5f, 3.75f, 0f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if the object is a mask and has been hit once
+       // if (this.CompareTag("Mask") && numberOfHits >= 1 && numberOfHits < maxHits)
+       // {
+            // teleport?
+           // int i = Random.Range(0, maskLocations.Length);
+           // transform.position = maskLocations[i].position;
+      //  }
     }
 
     // Collision rather than Trigger so the ball actually collides
@@ -34,10 +50,11 @@ public class DestroyBrick : MonoBehaviour
     {
         // Number of hits goes up every time the ball collides with the bricks
         numberOfHits++;
-
+        // Change sprite after first collision
         transform.GetComponent<SpriteRenderer>().sprite = oneHit;
 
-        if (this.transform.CompareTag ("Bricks"))
+        // Powerups
+        if (this.transform.CompareTag("Bricks"))
         {
             int randomChance = Random.Range(1, 101);
 
@@ -47,11 +64,10 @@ public class DestroyBrick : MonoBehaviour
             }
         }
         
-
+        // If brick is hit max # of times, update score and destroy brick
         if (numberOfHits >= maxHits)
         {
             gameMaster.UpdateScore(+brickValue);
-
             Destroy(this.gameObject);
         }
     }
